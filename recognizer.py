65cb65bdcbe096b75176ee128d16b54477bebfc6
Kostis Dimos <kostisd83@gmail.com>
2022-09-05 21:49:30 +0300

    len check in def recognizer instead of call in main

--- recognizer.py
+++ recognizer.py
@@ -21,25 +21,24 @@ def searchText(path):
 
 # Starting searching from the longest ngram
 def recognizer(sentence, ngram_list, filelist):
-    #matches = {}
-    match=()
     match_found = False
-    for ngram in ngram_list[::-1]:
-        if match_found == False and len(sentence) > 0:
-            for file in filelist:
-                textfile = open(file, 'r')
-                filetext = textfile.read()
-                textfile.close()
-                ngram_string = " ".join(ngram).strip()
-                pattern = re.compile(ngram_string)
+    while len(sentence) > 0:
+        for ngram in ngram_list[::-1]: # My only job is to get an utterance and find a match. If no match then what?
+            if match_found == False and len(sentence) > 0:
+                for file in filelist:
+                    textfile = open(file, 'r')
+                    filetext = textfile.read()
+                    textfile.close()
+                    ngram_string = " ".join(ngram).strip()
+                    pattern = re.compile(ngram_string)
 
-                if pattern.search(filetext):
-                    match_found = True
-                    match=(ngram_string, file)
+                    if pattern.search(filetext):
+                        match_found = True
+                        match=(ngram_string, file)
+                        return [ngram_string, match]
+        match = (sentence, "NOMATCH")
+        return [ngram_string, match]
 
-                    return [ngram_string, match, True]
 
-    match = (sentence, "NOMATCH")
-    return [sentence, match, False]
 
 

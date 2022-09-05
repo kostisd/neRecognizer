4b90bd99fa7fcc3f66e1891dde3a1acce92f8782
Kostis Dimos <kostisd83@gmail.com>
2022-09-05 14:12:45 +0300

    config for v2

---
+++ config.py
@@ -0,0 +1,7 @@
+dev_size=0.2
+input_data="data/ontonotes_en_name_entity.tsv"
+string_col='string' # name of text column
+entities_col='type'
+ent_min_n = 3 # remove entities if n < 3
+train=True
+test=True

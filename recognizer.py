e64def4fe020965e4fd9522526ee4698b47de817
Kostis Dimos <kostisd83@gmail.com>
2022-09-06 16:38:43 +0300

    moved search code to searchText function

--- recognizer.py
+++ recognizer.py
@@ -14,20 +14,40 @@ def make_ngrams(sentence):
         ngram += 1
     return ngram_list
 
-def searchText(path):
+def searchText(ngram_string, path):
+    pwd = os.getcwd()
     os.chdir(path)
-    filelist = os.listdir()
-    return filelist
+    entities_list = os.listdir()
+    match_found = False
+    for entity in entities_list:
+        textfile = open(entity, 'r')
+        filetext = textfile.read()
+        textfile.close()
+        #ngram_string = " ".join(ngram).strip()
+        #pattern = re.compile(ngram_string)
+        pattern = re.compile(ngram_string)
+        #match_found = pattern.search(filetext)
+        match_found = True if pattern.findall(filetext) else False # CHANGED TO FIND FROM SEARCH SOOOOOOOOS
+        if match_found:
+            print(match_found, entity, pattern)
+
+        #if pattern.search(filetext):
+        #    match_found = True
+        #print(match_found, entity)
+        #else:
+        #quit()
+    os.chdir(pwd)
+
+    return [match_found, entity]
 
 # Starting searching from the longest ngram
-def recognizer(sentence, filelist):
+def recognizer(sentence):
     original_sentence = sentence
     string_dict = {}
     entity_dict = {}
-    #print("Original sentence", sentence)
     while len(sentence.strip()) > 0:
         ngram_list = make_ngrams(sentence.strip())
-        sentence, match = match_finder(sentence, ngram_list, filelist)
+        sentence, match = match_finder(sentence, ngram_list)
         string_matched = match[0]
         entity_matched = match[1]
         start_index_matched = original_sentence.find(string_matched)
@@ -37,8 +57,6 @@ def recognizer(sentence, filelist):
         string_dict[start_index_matched] = string_matched
         entity_dict[start_index_matched] = entity_matched
 
-        #print("Table: ", string_matched, entity_matched, start_index_matched, span_matched)
-        #print("Trimmed Sentence ", sentence)
     a = sorted(string_dict.keys())
     b = sorted(string_dict.items())
     c = sorted(entity_dict.keys())
@@ -48,58 +66,42 @@ def recognizer(sentence, filelist):
     prev_index = 0
 
     for word, entity in zip(b, d):
-        #print("Entity: ", entity, " Word: ", word)
-        print("Prev: " , prev_index)
-        print("Curr", entity[0])
-        print("Steps: ", entity[0] - prev_index -1)
-        #print("Current loc: ", entity[0] - prev_index)
         str += (" " + word[1])
         ents = ents[:entity[0]]  + entity[1] + ents[entity[0] + 1:]
         #ents += (("#"*(entity[0] - prev_index - 1)) + entity[1])
         prev_index = entity[0]
-
-
-        #str += (" " + word[0][1])
-        #ent += (" " + str(entity[1][1]))
-    print(str)
-    print(ents)
-
-    #print(str)
-    #print(a)
-    # print(b)
-    # print(c)
-    # print(d)
-    # entity_dict = sorted(entity_dict)
-    # #print("Dictionary: ", string_dict)
-    #print(entity_dict)
-
-
+  #  print(str)
+  #  print(ents)
     return [string_dict, entity_dict]
 
-def match_finder(sentence, ngram_list, entities_list):
+def match_finder(sentence, ngram_list):
     origial_sentence = sentence
     #print("MATCH_FINDER:", origial_sentence, sentence)
     match_found = False
     for ngram in ngram_list[::-1]:
         if match_found == False:
-
+            ngram_string = " ".join(ngram).strip()
             # search start
-            for entity in entities_list:
-                textfile = open(entity, 'r')
-                filetext = textfile.read()
-                textfile.close()
-                ngram_string = " ".join(ngram).strip()
-                pattern = re.compile(ngram_string)
+            #pattern = rcg.searchText("dictionaries/")
+            # for entity in entities_list:
+            #     textfile = open(entity, 'r')
+            #     filetext = textfile.read()
+            #     textfile.close()
+            #     ngram_string = " ".join(ngram).strip()
+            #     pattern = re.compile(ngram_string)
             # end of search
+            match_found, entity = searchText(ngram_string, "dictionaries/")
+           # print(searchText(ngram_string, "dictionaries/"))
+            #quit()
+             #   if pattern.search(filetext):
+            if match_found: # check if match found
+                # Remove match from sentence and repeat
+                ngram_length = (len(ngram_string))
+                ngram_start = sentence.find(ngram_string)
 
-                if pattern.search(filetext):
-                    match_found = True
-                    # Remove match from sentence and repeat
-                    ngram_length = (len(ngram_string))
-                    ngram_start = sentence.find(ngram_string)
-                    match = (ngram_string, entity, ngram_start, ngram_length)
-                    sentence = trim_sentece(sentence, ngram_length, ngram_start)
-                    return [sentence, match]
+                match = (ngram_string, entity, ngram_start, ngram_length)
+                sentence = trim_sentece(sentence, ngram_length, ngram_start)
+                return [sentence, match]
 
     # if NO MATCH
     entity = "NOMATCH"

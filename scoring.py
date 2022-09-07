7844c4380ee57d653b78eb14667e1710781cfc08
Kostis Dimos <kostisd83@gmail.com>
2022-09-07 19:23:05 +0300

    sorting devset and removing NA from subtree_id

--- scoring.py
+++ scoring.py
@@ -1,4 +1,24 @@
 import pandas as pd
+import re
+
+def scoring(rcg_output, train_data, results_table):
+    id = re.sub(r'^.*?@', '', rcg_output[0])
+    ngram_list = rcg_output[1]
+    entity_list = rcg_output[2]
+
+    # dropna because many subtree_ids are missing
+    train_data = train_data.dropna(subset = ['subtree_id'])
+    train_data = train_data[train_data['subtree_id'].str.contains(id)]
+
+   if train_data:
+        for ngram, entity in zip(ngram_list, entity_list):
+            
+    else:
+        return
+    print("ID: " , id)
+    print(train_data)
+
+
 
 def calc_accuracy(matches, testset, results_table):
     corr = 0
@@ -13,6 +33,7 @@ def calc_accuracy(matches, testset, results_table):
     recall = corr / (corr + nomatch)
     return [accuracy, recall, results_table]
 
-def make_results_table():
-    results_table = pd.DataFrame(columns=['string', 'span', 'type', 'hyp', 'result'])
-    return results_table
+def make_table():
+    table = pd.DataFrame(columns=['id', 'string', 'span', 'type', 'hyp', 'result'])
+   # results_table = pd.DataFrame(columns=['string', 'span', 'type', 'hyp', 'result'])
+    return table

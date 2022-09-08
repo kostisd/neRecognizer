1eea473e47b99db5a827dea9173ffe70769a6e18
Kostis Dimos <kostisd83@gmail.com>
2022-09-08 17:26:14 +0300

    queue setup done

--- main.py
+++ main.py
@@ -73,36 +73,36 @@ if __name__ == '__main__':
           ngram = rcg_output[1]
           entity = rcg_output[2]
 
-          local_fp, local_tp, local_fn, local_tn = scr.scoring(rcg_output, train_data)
-          fp += local_fp
-          tp += local_tp
-          fn += local_fn
-          tn += local_tn
-          # Cover for division by zero
-          local_precision = round(local_tp / (local_tp + local_fp), 2) if (local_tp + local_fp) > 0 else "NA"
-          local_recall = round(local_tp / (local_tp + local_fn), 2) if (local_tp + local_fn) > 0 else "NA"
-          local_results = ["precision: ", local_precision, "recall: ", local_recall]
-          local_counts = str("tp: " + str(local_tp) + ", fp: " + str(local_fp) + ", tn: " + str(local_tn) + ", fn: " + str(local_fn))
-          local_results.append(local_counts)
+          # local_fp, local_tp, local_fn, local_tn = scr.scoring(rcg_output, train_data)
+          # fp += local_fp
+          # tp += local_tp
+          # fn += local_fn
+          # tn += local_tn
+          # # Cover for division by zero
+          # local_precision = round(local_tp / (local_tp + local_fp), 2) if (local_tp + local_fp) > 0 else "NA"
+          # local_recall = round(local_tp / (local_tp + local_fn), 2) if (local_tp + local_fn) > 0 else "NA"
+          # local_results = ["precision: ", local_precision, "recall: ", local_recall]
+          # local_counts = str("tp: " + str(local_tp) + ", fp: " + str(local_fp) + ", tn: " + str(local_tn) + ", fn: " + str(local_fn))
+          # local_results.append(local_counts)
 
-          with io.open('augmented_sentences.csv', 'a') as f:
-              write = csv.writer(f)
-              write.writerow("")
-              write.writerow(local_results)
-              write.writerow(ngram)
-              write.writerow(entity)
-          f.close()
+          # with io.open('augmented_sentences.csv', 'a') as f:
+          #     write = csv.writer(f)
+          #     write.writerow("")
+          #     write.writerow(local_results)
+          #     write.writerow(ngram)
+          #     write.writerow(entity)
+          # f.close()
 
-      print('Done')
-
-      precision = round(tp / (tp + fp), 2) if (tp + fp) > 0 else "NA"
-      recall = round(tp / (tp + fn), 2) if (tp + fn) > 0 else "NA"
-
-      print("\n" + print_line)
-      print("  Results ")
-      print(print_line)
-      print("\nPrecision: ", precision)
-      print("Recall:    ", recall , "\n")
+      # print('Done')
+      #
+      # precision = round(tp / (tp + fp), 2) if (tp + fp) > 0 else "NA"
+      # recall = round(tp / (tp + fn), 2) if (tp + fn) > 0 else "NA"
+      #
+      # print("\n" + print_line)
+      # print("  Results ")
+      # print(print_line)
+      # print("\nPrecision: ", precision)
+      # print("Recall:    ", recall , "\n")
 
 
 
--- recognizer.py
+++ recognizer.py
@@ -17,6 +17,33 @@ def make_ngrams(sentence):
         ngram += 1
     return ngram_list
 
+def trim_sentece(sentence_queue, sentence, ngram_start, ngram_length):
+    print("TRIM INFO: ", sentence, len(sentence), ngram_start, ngram_length)
+    if ngram_start == 0:  # match is at start of utt
+        #sentence = sentence[ngram_start + ngram_length:len(sentence)]  # removed -1 ,it was del last char
+        # keep an eye on this change
+        sentence = sentence[ngram_start + ngram_length:]  # removed -1 ,it was del last char
+        sentence_queue.append(sentence)
+    elif (ngram_start + ngram_length) >= len(sentence):  # match is at the end of utt (KEEP AN EYE AT >
+        sentence = sentence[0:ngram_start]
+        sentence_queue.append(sentence)
+    elif ngram_start > 0 and (ngram_start + ngram_length) < len(sentence):  # match in the middle
+        # MAJOR CHANGE HERE
+        sentence1 = sentence[0:ngram_start]
+        sentence2 = sentence[(ngram_start + ngram_length):]
+        sentence_queue.extend([sentence1, sentence2])
+        #sentence_queue.append(sentence1)
+        #sentence_queue.append(sentence2)
+    else:
+        print("ERROR rcg.trim_sentence: Something is wrong with indexing this ngram")
+        quit()
+
+    return sentence_queue
+
+# sentence = sentence[0:ngram_start] + sentence[(ngram_start + 1 + ngram_length):len(sentence) + 1]
+# sentence2 = entence[ngram_start + ngram_length:]
+
+
 def searchText(ngram_string, path):
     # change dirs to access dictionaries
     pwd = os.getcwd()
@@ -51,86 +78,61 @@ def searchText(ngram_string, path):
 
     return [match_found, match_entity]
 
-def trim_sentece(sentence, ngram_length, ngram_start):
-    if ngram_start == 0:  # match is at start of utt
-        #sentence = sentence[ngram_start + ngram_length:len(sentence)]  # removed -1 ,it was del last char
-        # keep an eye on this change
-        sentence = sentence[ngram_start + ngram_length:]  # removed -1 ,it was del last char
-    elif (ngram_start + ngram_length) >= len(sentence):  # match is at the end of utt (KEEP AN EYE AT >
-        sentence = sentence[0:ngram_start]
-    elif ngram_start > 0 and (ngram_start + ngram_length) < len(sentence):  # match in the middle
-        # MAJOR CHANGE HERE
-        sentence1 = sentence[0:ngram_start]
-        
-        sentence2 = sentence[(ngram_start + ngram_length):]
-        #sentence = sentence[0:ngram_start] + sentence[(ngram_start + 1 + ngram_length):len(sentence) + 1]
-        #sentence2 = entence[ngram_start + ngram_length:]
-    else:
-        print("ERROR rcg.trim_sentence: Something is wrong with indexing this ngram")
-        quit()
 
-    return sentence
-
-def match_finder(sentence, ngram_list):
-    original_sentence = sentence
-    match_found = False
-    for ngram in ngram_list[::-1]:
-        if match_found == False:
+def match_finder(ngram_list):
+    match_found = False # should stop searching if True
+    for ngram in ngram_list[::-1]: # starting from the longest ngram
+        if match_found is False:
             ngram_string = " ".join(ngram).strip()
-
             match_found, entity = searchText(ngram_string, "dictionaries/")
-
             if match_found: # check if match found
-                # Remove match from sentence and repeat
-                ngram_length = (len(ngram_string))
-                ngram_start = original_sentence.find(ngram_string)
-
-                match = (ngram_string, entity, ngram_start, ngram_length)
-                sentence = trim_sentece(sentence, ngram_length, ngram_start)
-                return [sentence, match]
-
+                match = (ngram_string, entity) # here we just pack the match info and pass it over
+                return match
     # if NO MATCH
     entity = "NOMATCH"
-    ngram_length = (len(ngram_string))
-    ngram_start = original_sentence.find(ngram_string)
-    match = (ngram_string, entity, ngram_start, ngram_length)
-    sentence = trim_sentece(sentence, ngram_length, ngram_start)
+    match = (ngram_string, entity)
+    return match
 
-    return [sentence, match]
-
-# Starting searching from the longest ngram
 def recognizer(input_list):
+    # input_list contains the id at [0] and the sentence as string at [1]
+    print("Starting Recognizer")
     original_sentence = input_list[1]
-    sentence = original_sentence
+    sentence_queue = [] # we will be adding trimmed sentences to tag
+    sentence = original_sentence # we'll work with sentence later
+    sentence_queue.append(sentence)
+    print("Queue 0: ", sentence_queue)
     id = input_list[0]
-    string_dict = {}
-    entity_dict = {}
-    while len(sentence.strip()) > 0:
-        ngram_list = make_ngrams(sentence.strip())
-        sentence, match = match_finder(sentence, ngram_list)
-        string_matched = match[0]
-        entity_matched = match[1]
-        # span_matched = match[3]
+    string_dict = {} # add all matched ngrams here
+    entity_dict = {} # add all found entities here
+    if sentence_queue: # if sentence_queue is empty, we are done!
+        sentence = sentence_queue.pop(0)
+        while len(sentence.strip()) > 0: # this will stop the matching and trimming cycle
+            print("QUEUE 1: ", sentence_queue)
+            ngram_list = make_ngrams(sentence.strip()) # we re-do the ngrams after every match
+            match = match_finder(ngram_list) # sentence here is a new, trimmed sub-sentence
+            string_matched, entity_matched = match[0], match[1] # match_finder just sends the matched ngram and it's tag
+            start_index_matched = original_sentence.find(string_matched) # find match in original
+            string_dict[start_index_matched] = string_matched # store matched string
+            entity_dict[start_index_matched] = entity_matched # store tag of string
+            print("Sentence to be trimmed: ", sentence)
+            sentence_queue = trim_sentece(sentence_queue, sentence, start_index_matched, len(string_matched)) # returns queue
+            print("QUEUE 2: ", sentence_queue)
+            sentence = sentence_queue.pop(0)
+            print("Sentence after trimmed: ", sentence, start_index_matched, len(string_matched))
+            print("string_matched: ", string_matched)
 
-        start_index_matched = original_sentence.find(string_matched)
-        string_dict[start_index_matched] = string_matched
-        print(string_dict)
-        entity_dict[start_index_matched] = entity_matched
+        words = sorted(string_dict.items())
+        print("Sorted: ", words)
+        entities = sorted(entity_dict.items())
+        #words_indexes = sorted(string_dict.keys())
+        #entities_indexes = sorted(entity_dict.keys())
 
-    words = sorted(string_dict.items())
-    print("Sorted: ", words)
-    entities = sorted(entity_dict.items())
-    #words_indexes = sorted(string_dict.keys())
-    #entities_indexes = sorted(entity_dict.keys())
-
-    word_list = []
-    entity_list = []
-    for word, entity in zip(words, entities):
-        word_list.append(word[1])
-        entity_list.append(entity[1])
+        word_list = []
+        entity_list = []
+        for word, entity in zip(words, entities):
+            word_list.append(word[1])
+            entity_list.append(entity[1])
 
     #print(tabulate([word_list, entity_list]))
 
     return (id, word_list, entity_list)
-    #return [string_dict, entity_dict]
-

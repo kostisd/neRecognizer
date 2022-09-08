dc0d751e0d467af53fd3652cc76c617eece32de5
Kostis Dimos <kostisd83@gmail.com>
2022-09-08 18:38:50 +0300

    searchText2 added for matching

--- recognizer.py
+++ recognizer.py
@@ -18,7 +18,7 @@ def make_ngrams(sentence):
     return ngram_list
 
 def trim_sentece(sentence_queue, sentence, ngram_start, ngram_length):
-    print("TRIM INFO: ", sentence, len(sentence), ngram_start, ngram_length)
+   # print("TRIM INFO: ", sentence, len(sentence), ngram_start, ngram_length)
     if ngram_start == 0:  # match is at start of utt
         #sentence = sentence[ngram_start + ngram_length:len(sentence)]  # removed -1 ,it was del last char
         # keep an eye on this change
@@ -42,6 +42,30 @@ def trim_sentece(sentence_queue, sentence, ngram_start, ngram_length):
 
 # sentence = sentence[0:ngram_start] + sentence[(ngram_start + 1 + ngram_length):len(sentence) + 1]
 # sentence2 = entence[ngram_start + ngram_length:]
+
+def searchText2(ngram_string, path):
+    pwd = os.getcwd()
+    os.chdir(path)
+    entities_list = os.listdir()
+
+    match_entity = "NOMATCH"
+    match_found = False
+    ngram_string = ngram_string.strip()
+    ngram_string = "^" + ngram_string + "$"
+    for entity in entities_list:
+        with open(entity, 'r') as f:
+            for line in f.readlines():
+                found = re.findall(ngram_string, line)
+                if found:
+                    match_found = True
+                    match_entity = entity
+                    f.close()
+                    os.chdir(pwd)  # Return to default pwd
+                    return [match_found, match_entity]
+            f.close()
+    os.chdir(pwd) # Return to default pwd
+
+    return [match_found, match_entity]
 
 
 def searchText(ngram_string, path):
@@ -84,7 +108,7 @@ def match_finder(ngram_list):
     for ngram in ngram_list[::-1]: # starting from the longest ngram
         if match_found is False:
             ngram_string = " ".join(ngram).strip()
-            match_found, entity = searchText(ngram_string, "dictionaries/")
+            match_found, entity = searchText2(ngram_string.strip(), "dictionaries/")
             if match_found: # check if match found
                 match = (ngram_string, entity) # here we just pack the match info and pass it over
                 return match
@@ -95,36 +119,35 @@ def match_finder(ngram_list):
 
 def recognizer(input_list):
     # input_list contains the id at [0] and the sentence as string at [1]
-    print("Starting Recognizer")
     original_sentence = input_list[1]
     sentence_queue = [] # we will be adding trimmed sentences to tag
     sentence = original_sentence # we'll work with sentence later
     sentence_queue.append(sentence)
-    print("Queue 0: ", sentence_queue)
+ #   print("Queue 0: ", sentence_queue)
     id = input_list[0]
     string_dict = {} # add all matched ngrams here
     entity_dict = {} # add all found entities here
     if sentence_queue: # if sentence_queue is empty, we are done!
         sentence = sentence_queue.pop(0)
         while len(sentence.strip()) > 0: # this will stop the matching and trimming cycle
-            print("QUEUE 1: ", sentence_queue)
+          #  print("QUEUE 1: ", sentence_queue)
             ngram_list = make_ngrams(sentence) # we re-do the ngrams after every match
             match = match_finder(ngram_list) # sentence here is a new, trimmed sub-sentence
             string_matched, entity_matched = match[0], match[1] # match_finder just sends the matched ngram and it's tag
             start_index_matched = original_sentence.find(string_matched) # FOR SORTING - NOT TRIMMING find match in original
             string_dict[start_index_matched] = string_matched # store matched string
             entity_dict[start_index_matched] = entity_matched # store tag of string
-            print("Sentence to be trimmed: ", sentence)
+            #print("Sentence to be trimmed: ", sentence)
 
             trim_index_matched = sentence.find(string_matched) # FOR SORTING - NOT TRIMMING find match in original
             sentence_queue = trim_sentece(sentence_queue, sentence, trim_index_matched, len(string_matched)) # returns queue
-            print("QUEUE 2: ", sentence_queue)
+          #  print("QUEUE 2: ", sentence_queue)
             sentence = sentence_queue.pop(0)
-            print("Sentence after trimmed: ", sentence, start_index_matched, len(string_matched))
-            print("string_matched: ", string_matched)
+           # print("Sentence after trimmed: ", sentence, start_index_matched, len(string_matched))
+            #print("string_matched: ", string_matched)
 
         words = sorted(string_dict.items())
-        print("Sorted: ", words)
+        # print("Sorted: ", words)
         entities = sorted(entity_dict.items())
         #words_indexes = sorted(string_dict.keys())
         #entities_indexes = sorted(entity_dict.keys())
@@ -135,6 +158,6 @@ def recognizer(input_list):
             word_list.append(word[1])
             entity_list.append(entity[1])
 
-    #print(tabulate([word_list, entity_list]))
+    print(tabulate([word_list, entity_list]))
 
     return (id, word_list, entity_list)

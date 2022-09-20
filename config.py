a6994a1f72028548facfd64d4bec9965a3c03825
Kostis Dimos <kostisd83@gmail.com>
2022-09-20 18:00:42 +0300

    removed dev size from config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-dev_size=0.2
 # Train data
-train_tsv="data/ontonotes_en_name_entity.tsv"
-string_col='string' # name of text column
-entities_col='type'
+train_tsv = "data/ontonotes_en_name_entity.tsv"
+string_col = 'string' # name of text column
+entities_col = 'type'
 
 # Dev data
-original_dev_tsv="data/ontonotes_en_token.tsv"
+original_dev_tsv = "data/ontonotes_en_token.tsv"
 
 # Gerneral
 ent_min_n = 3 # remove entities if n < 3
+
 train=False
 test=True
